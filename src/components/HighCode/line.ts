f9011e9e0d2cba846359eaf5aad65d34a0f37ab5
INS6+ <feiyuchuixue@163.com>
2024-10-10 11:11:31 +0800

    修复：代码预览组件行号展示问题

--- src/components/HighCode/line.ts
+++ src/components/HighCode/line.ts
@@ -1,26 +1,38 @@
 import './code.scss'
+
 const vCode = {
   mounted(el: any) {
-    //获取代码片段
-    let code = el.querySelector('code.hljs')
-    let pre = el.querySelector('pre')
-    let html = code?.innerHTML
-    let size = html.split('\n').length
+    buildLineNumber(el)
+  },
+  updated(el: any) {
+    buildLineNumber(el)
+  }
+}
 
-    //插入行数
-    let ul = document.createElement('ul')
-    for (let i = 1; i <= size; i++) {
-      let li = document.createElement('li')
-      li.innerText = i + ''
-      ul.appendChild(li)
-    }
+const buildLineNumber = (el: any) => {
+  //获取代码片段
+  let code = el.querySelector('code.hljs')
+  let pre = el.querySelector('pre')
+  let html = code?.innerHTML
+  let size = html.split('\n').length
+  let codeNumber = el.querySelector('.hljs-code-number')
+  if (codeNumber) {
+    el.removeChild(codeNumber)
+  }
+  console.log('codeNumber',codeNumber)
+  //插入行数
+  let ul = document.createElement('ul')
+  for (let i = 1; i <= size; i++) {
+    let li = document.createElement('li')
+    li.innerText = i + ''
+    ul.appendChild(li)
+  }
 
-    ul.classList.add('hljs-code-number')
+  ul.classList.add('hljs-code-number')
 
-    // 确保 pre 是 el 的子元素后再进行插入
-    if (pre && pre.parentNode === el) {
-      el.insertBefore(ul, pre)
-    }
+  // 确保 pre 是 el 的子元素后再进行插入
+  if (pre && pre.parentNode === el) {
+    el.insertBefore(ul, pre)
   }
 }
 
