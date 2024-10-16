21554480b8b3e9be745a7748c4067e46e0e83311
INS6+ <feiyuchuixue@163.com>
2024-10-16 11:12:53 +0800

    优化: useSelection组件添加对number数组的支持

--- src/hooks/useSelection.ts
+++ src/hooks/useSelection.ts
@@ -6,11 +6,11 @@ import { ref, computed } from 'vue'
  * */
 export const useSelection = (rowKey: string = 'id') => {
   const isSelected = ref(false)
-  const selectedList = ref<{ [key: string]: any }[]>([])
+  const selectedList = ref<{ [key: string | number]: any }[]>([])
 
   // 当前选中的所有 ids 数组
   const selectedListIds = computed(() => {
-    const ids: string[] = []
+    const ids: (string | number)[] = []
     selectedList.value.forEach((item) => ids.push(item[rowKey]))
     return ids
   })
