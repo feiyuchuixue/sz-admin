ad5098a99357c9f25424122ad637fb52a8134645
INS6+ <feiyuchuixue@163.com>
2025-12-02 17:17:38 +0800

    依赖升级 && 更新protable组件部分类型问题

--- src/components/ProTable/interface/index.ts
+++ src/components/ProTable/interface/index.ts
@@ -17,7 +17,7 @@ export interface ProTableProps {
   initParam?: any; // 初始化请求参数 ==> 非必传（默认为{}）
   border?: boolean; // 是否带有纵向边框 ==> 非必传（默认为true）
   toolButton?: ('refresh' | 'setting' | 'search')[] | boolean; // 是否显示表格功能按钮 ==> 非必传（默认为true）
-  rowKey: string; // 行数据的 Key，用来优化 Table 的渲染，当表格数据多选时，所指定的 id ==> 非必传（默认为 id）
+  rowKey?: string; // 行数据的 Key，用来优化 Table 的渲染，当表格数据多选时，所指定的 id ==> 非必传（默认为 id）
   searchCol?: number | Record<'xs' | 'sm' | 'md' | 'lg' | 'xl', number>; // 表格搜索项 每列占比配置 ==> 非必传 { xs: 1, sm: 2, md: 2, lg: 3, xl: 4 }
   loadingTime?: number; // 表格数据加载时间 ==> 非必传（默认为200）
 }
