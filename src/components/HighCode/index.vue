e87280a42915494730cc56f1a3cdbb99d45ba3f8
INS6+ <feiyuchuixue@163.com>
2026-05-21 16:26:15 +0800

    优化：HighCode组件，添加下载的支持

--- src/components/HighCode/index.vue
+++ src/components/HighCode/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="hljs-container">
     <div class="hljs-header" :codetype="title">
-      <div class="hljs-copy" @click="handleCopy">
+      <div class="hljs-copy">
         <el-button type="primary" link @click="handleCopy" :icon="DocumentCopy" class="hljs-copy">
           {{ copyButtonText }}
+        </el-button>
+        <el-button type="primary" link @click="handleDownload" :icon="Download" class="hljs-copy">
+          {{ downloadButtonText }}
         </el-button>
       </div>
     </div>
@@ -14,7 +17,7 @@
 </template>
 <script setup lang="ts">
 import { ref } from 'vue';
-import { DocumentCopy } from '@element-plus/icons-vue';
+import { DocumentCopy, Download } from '@element-plus/icons-vue';
 import vCode from './line';
 
 defineOptions({
@@ -26,21 +29,19 @@ const props = withDefaults(defineProps<HighCodeProps>(), {
   title: ''
 });
 const copyButtonText = ref<string>('复制');
+const downloadButtonText = ref<string>('下载');
 
 export interface HighCodeProps {
-  language: string; // 语言
-  code: string; // 要填充的代码块
-  title: string; // 要展示的title
+  language: string;
+  code: string;
+  title: string;
 }
 
-// 复制到剪贴板方法
 const handleCopy = async () => {
   try {
     if (navigator.clipboard) {
-      // 安全域
       await navigator.clipboard.writeText(props.code);
     } else {
-      // 非安全域，回退到 document.execCommand
       const tempTextarea = document.createElement('textarea');
       tempTextarea.value = props.code;
       document.body.appendChild(tempTextarea);
@@ -48,13 +49,26 @@ const handleCopy = async () => {
       document.execCommand('copy');
       document.body.removeChild(tempTextarea);
     }
-    copyButtonText.value = '复制成功！';
+    copyButtonText.value = '复制成功';
     setTimeout(() => {
       copyButtonText.value = '复制';
-    }, 2000); // 2000 毫秒后还原为 '复制'
+    }, 2000);
   } catch (err) {
-    console.error('复制操作不被支持或失败: ', err);
+    console.error('复制操作不被支持或失败', err);
   }
+};
+
+const handleDownload = () => {
+  const filename = props.title || 'download.txt';
+  const blob = new Blob([props.code], { type: 'text/plain;charset=utf-8' });
+  const url = window.URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  window.URL.revokeObjectURL(url);
 };
 </script>
 
