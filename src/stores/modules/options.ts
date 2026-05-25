d15ebd5b2aee9b5eabdb9607710dfce752cfba92
INS6+ <feiyuchuixue@163.com>
2026-05-25 10:54:21 +0800

    优化：[字典管理] - 增强静态字典加载功能，优化字典加载器和缓存机制

--- src/stores/modules/options.ts
+++ src/stores/modules/options.ts
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia';
-import { getAllDict, getDictByCode } from '@/api/modules/system/dict';
+import { getDictByCode, getStaticDict } from '@/api/modules/system/dict';
 import piniaPersistConfig from '@/stores/helper/persist';
 import type { DictCustom } from '@/api/types/system/dict';
 import { ref } from 'vue';
@@ -9,27 +9,80 @@ export const useOptionsStore = defineStore(
   () => {
     const isLoaded = ref(false);
     const dictOptions = ref<Record<string, DictCustom[]>>({});
+    const loadedTypes = ref<Record<string, boolean>>({});
+    const expiredTypes = ref<Record<string, boolean>>({});
+    const loadingTypes = ref<Record<string, boolean>>({});
 
     async function getAllDictList() {
       if (isLoaded.value) return;
-      const { data } = await getAllDict();
-      dictOptions.value = data;
+      const { data } = await getStaticDict();
+      dictOptions.value = { ...dictOptions.value, ...data };
+      Object.keys(data).forEach(markLoaded);
       isLoaded.value = true;
     }
 
     function getDictOptions(type: string): DictCustom[] {
+      if (shouldLoad(type)) {
+        void ensureDictByCodes([type]).catch(() => undefined);
+      }
       return dictOptions.value[type] || [];
     }
 
     function setReloadOptions() {
       isLoaded.value = false;
+      Object.keys(loadedTypes.value).forEach(code => {
+        expiredTypes.value[code] = true;
+      });
     }
 
     async function getDictByCodes(typeCodes: string[]) {
-      const { data } = await getDictByCode({ typeCode: typeCodes });
-      typeCodes.forEach(code => {
-        dictOptions.value[code] = data[code];
+      await loadDictByCodes(typeCodes, true);
+    }
+
+    async function ensureDictByCodes(typeCodes: string[]) {
+      const pendingCodes = typeCodes.filter(shouldLoad);
+      await loadDictByCodes(pendingCodes, false);
+    }
+
+    function expireDictTypes(typeCodes?: string[]) {
+      const codes = typeCodes?.length ? typeCodes : Object.keys(loadedTypes.value);
+      codes.forEach(code => {
+        expiredTypes.value[code] = true;
       });
+    }
+
+    async function loadDictByCodes(typeCodes: string[], force: boolean) {
+      const requestCodes = normalizeTypeCodes(typeCodes).filter(code => {
+        return force ? !loadingTypes.value[code] : shouldLoad(code);
+      });
+      if (!requestCodes.length) return;
+      requestCodes.forEach(code => {
+        loadingTypes.value[code] = true;
+      });
+      try {
+        const { data } = await getDictByCode({ typeCode: requestCodes });
+        requestCodes.forEach(code => {
+          dictOptions.value[code] = data[code] || [];
+          markLoaded(code);
+        });
+      } finally {
+        requestCodes.forEach(code => {
+          loadingTypes.value[code] = false;
+        });
+      }
+    }
+
+    function shouldLoad(type: string): boolean {
+      return !!type && !loadingTypes.value[type] && (!loadedTypes.value[type] || expiredTypes.value[type]);
+    }
+
+    function markLoaded(type: string) {
+      loadedTypes.value[type] = true;
+      expiredTypes.value[type] = false;
+    }
+
+    function normalizeTypeCodes(typeCodes: string[]): string[] {
+      return Array.from(new Set(typeCodes.filter(Boolean)));
     }
 
     return {
@@ -38,7 +91,9 @@ export const useOptionsStore = defineStore(
       getAllDictList,
       getDictOptions,
       setReloadOptions,
-      getDictByCodes
+      getDictByCodes,
+      ensureDictByCodes,
+      expireDictTypes
     };
   },
   {
